6c50d6b27da4ee9610a879c02b8c26db928dc4a7
Ellie Pratt <ellie@skfl.tech>
2026-09-05 16:33:03 +1000

    feat: Setup initial gossipsub behavior

--- game/src/net.rs
+++ game/src/net.rs
@@ -1,22 +1,28 @@
-use std::{error::Error, time::Duration};
-
 use anyhow::Result;
 use futures::prelude::*;
+use futures::stream::StreamExt;
 use libp2p::swarm::NetworkBehaviour;
 use libp2p::{
-    Multiaddr, identify, kad,
+    Multiaddr, gossipsub, identify, kad,
     kad::{Mode, store::MemoryStore},
     mdns, noise, ping,
     swarm::SwarmEvent,
     tcp, yamux,
 };
+use std::{
+    collections::hash_map::DefaultHasher,
+    error::Error,
+    hash::{Hash, Hasher},
+    time::Duration,
+};
+use tokio::{io, io::AsyncBufReadExt, select};
 use tracing_subscriber::EnvFilter;
 
 #[derive(NetworkBehaviour)]
 struct GameNet {
-    mdns: mdns::tokio::Behaviour,  // Discover local peers
-    identify: identify::Behaviour, // Allows peer identity
-    // gossipsub: gossipsub::Behaviour, // Publish info across the network
+    mdns: mdns::tokio::Behaviour,     // Discover local peers
+    identify: identify::Behaviour,    // Allows peer identity
+    gossipsub: gossipsub::Behaviour,  // Publish info across the network
     kad: kad::Behaviour<MemoryStore>, // Store info on the network
 }
 
@@ -42,10 +48,33 @@ pub async fn main() -> Result<()> {
                 key.public().to_peer_id(),
                 MemoryStore::new(key.public().to_peer_id()),
             );
+            // To content-address message, we can take the hash of message and use it as an ID.
+            let message_id_fn = |message: &gossipsub::Message| {
+                let mut s = DefaultHasher::new();
+                message.data.hash(&mut s);
+                gossipsub::MessageId::from(s.finish().to_string())
+            };
+
+            // Set a custom gossipsub configuration
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
+                // signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .build()
+                .map_err(io::Error::other)?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+            // build a gossipsub network behaviour
+            let gossipsub = gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(key.clone()),
+                gossipsub_config,
+            )?;
+
             Ok(GameNet {
                 mdns,
                 identify,
                 kad,
+                gossipsub,
             })
         })?
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely.
